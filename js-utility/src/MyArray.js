c9aabb0a3a1891eedad06c5827a4f0b89a27194a
jscfping <jscfping@gmail.com>
2023-10-03 23:28:32 +0800

    feat: add js-utility for Array

---
+++ js-utility/src/MyArray.js
@@ -0,0 +1,78 @@
+export class MyArray extends Array {
+
+
+    Chunk(size) {
+        if (!Number.isInteger(size)) {
+            throw new TypeError('Size parameter should be an integer.');
+        }
+
+        const chunks = [];
+        for (let i = 0; i < this.length; i += size) {
+            chunks.push(this.slice(i, i + size));
+        }
+        return chunks;
+    }
+
+    Min(selector) {
+        if (typeof selector !== 'function') {
+            throw new TypeError('Selector should be a function.');
+        }
+        if (this.length === 0) {
+            throw new Error('Cannot get Min value from an empty array.');
+        }
+        const values = this.map(selector);
+        for (const val of values) {
+            if (typeof val !== 'number' || isNaN(val) || val === Infinity) {
+                throw new TypeError('The result of the selector function should be a finite number for each element.');
+            }
+        }
+        return Math.min(...values);
+    }
+
+    Max(selector) {
+        if (typeof selector !== 'function') {
+            throw new TypeError('Selector should be a function.');
+        }
+        if (this.length === 0) {
+            throw new Error('Cannot get Max value from an empty array.');
+        }
+        const values = this.map(selector);
+        for (const val of values) {
+            if (typeof val !== 'number' || isNaN(val) || val === Infinity) {
+                throw new TypeError('The result of the selector function should be a finite number for each element.');
+            }
+        }
+        return Math.max(...values);
+    }
+
+    Sum(selector) {
+        if (typeof selector !== 'function') {
+            throw new TypeError('Selector should be a function.');
+        }
+        if (this.length === 0) return 0;
+
+        const sumValue = this.reduce((acc, el) => acc + selector(el), 0);
+        if (typeof sumValue !== 'number' || isNaN(sumValue) || sumValue === Infinity) {
+            throw new TypeError('The result of the selector function should be a finite number.');
+        }
+        return sumValue;
+    }
+
+    GroupBy(keySelector) {
+        if (typeof keySelector !== 'function') {
+            throw new TypeError('Key selector should be a function.');
+        }
+        const result = {};
+        for (let i = 0; i < this.length; i++) {
+            const key = keySelector(this[i], i);
+            if (typeof key !== 'string') {
+                throw new TypeError('The key must be a string.');
+            }
+            if (!result[key]) {
+                result[key] = [];
+            }
+            result[key].push(this[i]);
+        }
+        return result;
+    }
+}
---
+++ js-utility/tests/MyArray.test.js
@@ -0,0 +1,145 @@
+import { expect } from 'chai';
+import { MyArray } from '../src/MyArray.js';
+
+describe('MyArray', () => {
+
+    describe('Chunk', () => {
+        it('should chunk array into smaller arrays of specified size', () => {
+            const myArr = new MyArray(1, 2, 3, 4, 5, 6, 7, 8);
+            const chunks = myArr.Chunk(3);
+            expect(chunks).to.deep.equal([
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8]
+            ]);
+        });
+
+        it('should throw error if size is not an integer', () => {
+            const myArr = new MyArray(1, 2, 3, 4, 5, 6, 7, 8);
+            expect(() => myArr.Chunk(2.5)).to.throw();
+            expect(() => myArr.Chunk('2')).to.throw();
+        });
+    });
+
+
+
+    describe('Min', () => {
+        it('should return the minimum number', () => {
+            const myArr = new MyArray(5, 3, 8);
+            const min = myArr.Min(el => el);
+            expect(min).to.equal(3);
+        });
+
+        it('should throw error if result is not a finite number', () => {
+            const myArr = new MyArray('5', '3', '8');
+            expect(() => myArr.Min(el => el)).to.throw(TypeError, 'The result of the selector function should be a finite number for each element.');
+        });
+
+        it('should throw error if selector is not a function', () => {
+            const myArr = new MyArray(5, 3, 8);
+            expect(() => myArr.Min('not a function')).to.throw(TypeError, 'Selector should be a function.');
+        });
+    });
+
+    describe('Max', () => {
+        it('should return the maximum number', () => {
+            const myArr = new MyArray(5, 3, 8);
+            const max = myArr.Max(el => el);
+            expect(max).to.equal(8);
+        });
+
+        it('should throw error if result is not a finite number', () => {
+            const myArr = new MyArray('5', '3', '8');
+            expect(() => myArr.Max(el => el)).to.throw(TypeError, 'The result of the selector function should be a finite number for each element.');
+        });
+
+        it('should throw error if selector is not a function', () => {
+            const myArr = new MyArray(5, 3, 8);
+            expect(() => myArr.Max('not a function')).to.throw(TypeError, 'Selector should be a function.');
+        });
+    });
+
+    describe('Sum', () => {
+        it('should return the sum of all elements', () => {
+            const myArr = new MyArray(5, 3, 8);
+            const sum = myArr.Sum(el => el);
+            expect(sum).to.equal(16);
+        });
+
+        it('should throw error if result is not a finite number', () => {
+            const myArr = new MyArray('5', '3', '8');
+            expect(() => myArr.Sum(el => el)).to.throw(TypeError, 'The result of the selector function should be a finite number.');
+        });
+
+        it('should throw error if selector is not a function', () => {
+            const myArr = new MyArray(5, 3, 8);
+            expect(() => myArr.Sum('not a function')).to.throw(TypeError, 'Selector should be a function.');
+        });
+
+        it('should return 0 for an empty array', () => {
+            const myArr = new MyArray();
+            const sum = myArr.Sum(el => el);
+            expect(sum).to.equal(0);
+        });
+    });
+
+    describe('GroupBy', () => {
+        it('should group elements by a given function', () => {
+            const myArr = new MyArray(
+                { age: 25, name: "alice" },
+                { age: 30, name: "bob" },
+                { age: 25, name: "charlie" }
+            );
+
+            const grouped = myArr.GroupBy(el => `${el.age}`);
+
+            expect(grouped).to.deep.equal({
+                "25": [{ age: 25, name: "alice" }, { age: 25, name: "charlie" }],
+                "30": [{ age: 30, name: "bob" }]
+            });
+        });
+
+        it('should throw error if key is not a string', () => {
+            const myArr = new MyArray({ age: 25 }, { age: 30 }, { age: 25 });
+            expect(() => myArr.GroupBy(el => el.age)).to.throw(TypeError, 'The key must be a string.');
+        });
+
+        it('should throw error if key selector is not a function', () => {
+            const myArr = new MyArray({ age: 25 }, { age: 30 }, { age: 25 });
+            expect(() => myArr.GroupBy('not a function')).to.throw(TypeError, 'Key selector should be a function.');
+        });
+
+        it('should return an empty object for an empty array', () => {
+            const myArr = new MyArray();
+            const grouped = myArr.GroupBy(el => el.someKey);
+            expect(grouped).to.deep.equal({});
+        });
+    });
+
+    describe('map', () => {
+        it('should correctly map over the elements', () => {
+            const myArr = new MyArray(1, 2, 3, 4);
+            const mappedArray = myArr.map(el => el * 2);
+            expect(mappedArray).to.deep.equal([2, 4, 6, 8]);
+        });
+
+        it('should throw error if provided argument is not a function', () => {
+            const myArr = new MyArray(1, 2, 3, 4);
+            expect(() => myArr.map('not a function')).to.throw(TypeError);
+        });
+    });
+
+    describe('filter', () => {
+        it('should correctly filter the elements based on the provided function', () => {
+            const myArr = new MyArray(1, 2, 3, 4);
+            const filteredArray = myArr.filter(el => el > 2);
+            expect(filteredArray).to.deep.equal([3, 4]);
+        });
+
+        it('should throw error if provided argument is not a function', () => {
+            const myArr = new MyArray(1, 2, 3, 4);
+            expect(() => myArr.filter('not a function')).to.throw(TypeError);
+        });
+    });
+
+});
