79447d4af0815352c13e99c5cb154de0a26bc453
jscfping <jscfping@gmail.com>
2024-03-03 20:23:17 +0800

    feat: add toJSON.js

---
+++ js/utility/src/toJSON.js
@@ -0,0 +1,39 @@
+
+
+function toJSON(object) {
+    const obj = {};
+
+    for (const key of Object.keys(object)) {
+        obj[key] = object[key];
+    }
+
+    const proto = Object.getPrototypeOf(object);
+    for (const key of Object.getOwnPropertyNames(proto)) {
+        const desc = Object.getOwnPropertyDescriptor(proto, key);
+        if (desc && typeof desc.get === "function") {
+            obj[key] = object[key];
+        }
+    }
+    return obj;
+}
+
+class A {
+    constructor() {
+      this.prop1 = 'value1';
+      this.prop2 = 'value2';
+    }
+  
+    get prop3() {
+      return "value3";
+    }
+  
+    method1() {
+      return this.prop1;
+    }
+  }
+  
+  const a = new A();
+  console.log(JSON.stringify(toJSON(a)));
+
+
+  
